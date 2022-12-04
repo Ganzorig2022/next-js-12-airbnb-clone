67079d5e42b27063f32cb22aa49172010b254d03
Ganzorig <ganzo.galaxy@gmail.com>
2022-12-04 20:35:29 +0800

    vercel env added

--- components/Map.js
+++ components/Map.js
@@ -12,7 +12,7 @@ const Map = () => {
       }}
       style={{ width: '100%', height: '100%' }}
       mapStyle='mapbox://styles/ganzorig2022/clb9abvrx006714s3jvojleoc' //"https://studio.mapbox.com/styles/ganzorig2022" mapbox site-aas custom style-nii url-aa huulj tawina.
-      mapboxAccessToken={process.env.MAPBOX_KEY}
+      mapboxAccessToken={process.env.MAPBOX_KEY} //vercel env
     />
   );
 };
